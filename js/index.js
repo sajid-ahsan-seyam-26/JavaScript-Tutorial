3550fb67b0f5e1a684052ff374d49f9219f6902e
sajid-ahsan-seyam-26 <sajidahsanseyamstudent@gmail.com>
2026-06-22 00:37:49 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// console.log("Hello, World!");
+// console.log("I like your home work");
+// window.alert("this is an alert");
+
+document.getElementById("myH1").textContent = "Hello";
+
+document.getElementById("myP").textContent = "I like Pizza";
